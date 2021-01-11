2f68a199b24df0050add9b7923cc3f51915bfdc5
Sergey Pimenov <sergey@pimenov.com.ua>
2021-01-11 23:34:23 +0200

    ...

--- src/browser.js
+++ src/browser.js
@@ -1,11 +1,9 @@
 import functions from "./functions";
-import Cake, {cake} from "./type";
 import getGlobalObject from "./helpers/common/get_global";
 
 Object.assign(Cake, functions);
 
 let global = getGlobalObject();
 
-global.cake = cake;
 global.Cake = Cake;
 
